9abea2bf573c6a2c466e48b4006d287f2f094e2a
Jun-Real <143982793+Jun-Real@users.noreply.github.com>
2024-12-20 23:54:46 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -342,7 +342,7 @@ app = FastAPI()
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000/"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
